3c99a14c870d9b6d6c2503d577f399f176696ac8
Zaheen04 <khanzaheen799@gmail.com>
2023-12-08 00:24:06 +0530

    test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");  //import json-server library
 const server=jsonServer.create();
-const router=jsonServer.router("database.json");
+const router=jsonServer.router("student_db.json");
 const middlewares= jsonServer.defaults();
 const port = process.env.PORT || 3001;
 server.use(middlewares);
